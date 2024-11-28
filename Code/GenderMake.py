fb1be9e88f44e619b96f52ea1a76e862acf38821
YEONGHUN-H <Hong77889931@gmail.com>
2024-11-29 00:24:51 +0900

    ..

---
+++ Code/GenderMake.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+input_file = 'myPC/input_data.csv' 
+output_file = 'myPC/output_data_with_gender.csv' 
+df = pd.read_csv(input_file)
+
+# Gender 열 추가 (Height 기준으로 1.70m 밑은 Female, 이상은 Male)
+df['Gender'] = df['Height'].apply(lambda x: 'Female' if x < 1.70 else 'Male')
+
+print(df.head(5))
+
+df.to_csv(output_file, index=False)
+print(f"가공 완료. 데이터가 '{output_file}'에 저장되었습니다.")
